f826b2271c482d4936bdd74d2ae647691de34175
kassem00 <kamsdonga@gmail.com>
2024-08-22 01:36:47 +0000

    au

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,15 +17,12 @@ class FIFOCache(BaseCaching):
         """ Assign the item to the dictionary """
         if key is not None and item is not None:
             if key in self.cache_data:
-                # If key already exists, just update the value and move it to the end
                 self.order.remove(key)
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Cache is full, remove the oldest item
                 oldest_key = self.order.pop(0)
                 del self.cache_data[oldest_key]
                 print("DISCARD: {}".format(oldest_key))
 
-            # Add the item to the cache and the order list
             self.cache_data[key] = item
             self.order.append(key)
 
