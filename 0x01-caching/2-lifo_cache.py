0729ebb54840dc181e68d94729a01cb47fb272b5
kassem00 <kamsdonga@gmail.com>
2024-08-22 01:56:25 +0000

    au

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" FIFOCache class """
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Assign the item to the dictionary """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = self.order.pop(len(self.order) - 1)
+                del self.cache_data[last_key]
+                print("DISCARD: {}".format(last_key))
+
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Retrieve the item from the dictionary """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
