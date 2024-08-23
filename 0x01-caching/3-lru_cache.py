fca86e0d8e2a22a566117fefe644c5a1c14128b7
kassem00 <kamsdonga@gmail.com>
2024-08-23 21:29:02 +0000

    au

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,25 +16,27 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """ Assign the item to the dictionary """
         if key is not None and item is not None:
-            # Update usage count
             if key in self.cache_data:
-                self.order.remove(key)
+                self.order.remove(key)  # Remove the old key
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Find the least recently used key (min usage count)
-                least_used_key = min(self.how_many_it_used, key=self.how_many_it_used.get)
-                self.order.remove(least_used_key)
+                # Find the least recently used key and discard it
+                least_used_key = self.order.pop(0)
                 del self.cache_data[least_used_key]
-                del self.how_many_it_used[least_used_key]
                 print("DISCARD: {}".format(least_used_key))
 
+            # Add the new key to the cache
             self.cache_data[key] = item
-            self.order.append(key)
+            self.order.append(key)  # Append to end as most recently used
             self.how_many_it_used[key] = self.how_many_it_used.get(key, 0) + 1
 
     def get(self, key):
         """ Retrieve the item from the dictionary """
         if key is None or key not in self.cache_data:
             return None
-        # Increment usage count
+        
+        # Move the key to the end to mark it as recently used
+        self.order.remove(key)
+        self.order.append(key)
         self.how_many_it_used[key] += 1
+
         return self.cache_data.get(key)
