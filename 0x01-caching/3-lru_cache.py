350cf6ad9605a272ada6a302f9115de489276a02
kassem00 <kamsdonga@gmail.com>
2024-08-23 21:34:07 +0000

    au

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,33 +10,44 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
-        self.order = []
-        self.how_many_it_used = {}
+        self.iteration_count = {}  # Dictionary to track iteration count of each key
 
     def put(self, key, item):
         """ Assign the item to the dictionary """
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.order.remove(key)  # Remove the old key
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Find the least recently used key and discard it
-                least_used_key = self.order.pop(0)
-                del self.cache_data[least_used_key]
-                print("DISCARD: {}".format(least_used_key))
+                # If the key exists, reset its counter
+                self.iteration_count[key] = 0
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    # Find the key with the highest iteration count (least recently used)
+                    lru_key = max(self.iteration_count, key=self.iteration_count.get)
+                    del self.cache_data[lru_key]
+                    del self.iteration_count[lru_key]
+                    print("DISCARD: {}".format(lru_key))
 
-            # Add the new key to the cache
+                # Add the new key to the cache and reset its counter
+                self.iteration_count[key] = 0
+
+            # Insert/update the key and value in the cache
             self.cache_data[key] = item
-            self.order.append(key)  # Append to end as most recently used
-            self.how_many_it_used[key] = self.how_many_it_used.get(key, 0) + 1
+
+            # Increment all other counters
+            for k in self.iteration_count:
+                if k != key:
+                    self.iteration_count[k] += 1
 
     def get(self, key):
         """ Retrieve the item from the dictionary """
         if key is None or key not in self.cache_data:
             return None
         
-        # Move the key to the end to mark it as recently used
-        self.order.remove(key)
-        self.order.append(key)
-        self.how_many_it_used[key] += 1
+        # Reset the counter of the accessed key to 0
+        self.iteration_count[key] = 0
+
+        # Increment all other counters
+        for k in self.iteration_count:
+            if k != key:
+                self.iteration_count[k] += 1
 
         return self.cache_data.get(key)
