1276017757927ca5e50b10dcd2c307dd75411f98
kassem00 <kamsdonga@gmail.com>
2024-08-23 21:43:18 +0000

    au

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ LRUCache class """
 
-
 from base_caching import BaseCaching
 
 
@@ -17,28 +16,35 @@ class LRUCache(BaseCaching):
         """ Assign the item to the dictionary """
         if key is not None and item is not None:
             if key in self.cache_data:
+                # If the key exists, reset its counter to 0
                 self.iteration_count[key] = 0
             else:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    key = self.iteration_count.get
-                    lru_key = max(self.iteration_count, key)
+                    # Find the key with the highest iteration count (least recently used)
+                    lru_key = max(self.iteration_count, key=self.iteration_count.get)
                     del self.cache_data[lru_key]
                     del self.iteration_count[lru_key]
                     print("DISCARD: {}".format(lru_key))
 
+                # Add the new key to the cache and initialize its counter to 0
                 self.iteration_count[key] = 0
 
+            # Insert/update the key and value in the cache
             self.cache_data[key] = item
 
+            # Increment the counters of all other keys
             for k in self.iteration_count:
                 if k != key:
                     self.iteration_count[k] += 1
-
     def get(self, key):
         """ Retrieve the item from the dictionary """
         if key is None or key not in self.cache_data:
             return None
+        
+        # Reset the counter of the accessed key to 0
         self.iteration_count[key] = 0
+
+        # Increment all other counters
         for k in self.iteration_count:
             if k != key:
                 self.iteration_count[k] += 1
