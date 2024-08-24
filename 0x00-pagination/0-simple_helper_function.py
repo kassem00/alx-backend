5fe63a88eb30eaf2b8b482456afa045d650565c4
kassem00 <kamsdonga@gmail.com>
2024-08-24 01:17:38 +0000

    au

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,11 @@
 
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """ 
-    index_range that takes two integer arguments page and page_size.
     """
+    index_range that takes two integer arguments page and page_size.
+    start_index = (page_number - 1) * page_size
+    end_index = page_number * page_size
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
