3160692b8e1f8dcfe5843ca0b437f5bb73a3a5f6
kassem00 <kamsdonga@gmail.com>
2024-08-27 23:13:19 +0000

    au

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,12 +3,11 @@
 
 
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
 
 
-@app.request("/")
+@app.route("/")
 def index():
     return render_template("templates/0-index.html")
 
