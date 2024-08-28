dff1508bba4286601aefc030f7208e534c2c061f
kassem00 <kamsdonga@gmail.com>
2024-08-28 15:23:21 +0000

    au

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -33,6 +33,7 @@ def get_locale():
         return user.locale
     return request.accept_languages.best_match(['en', 'fr'])
 
+
 if __name__ == "__main__":
     """ if function in main """
     app.run(debug=True, host="0.0.0.0")
