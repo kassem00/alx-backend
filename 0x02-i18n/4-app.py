3a3fc69c90ae489a38a31b370269c22d5cf05cc3
kassem00 <kamsdonga@gmail.com>
2024-08-29 00:02:22 +0000

    au

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Flask app with Babel for i18n and l10n."""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _, gettext, get_locale
+
+
+class Config:
+    """Flask app config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get user's locale."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+app.jinja_env.globals['get_locale'] = get_locale
+
+
+@app.route("/")
+def index() -> str:
+    """Render homepage."""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
