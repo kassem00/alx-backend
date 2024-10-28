b1126bccee3ad93ae1803952a2b9f3c5484a7415
kassem00 <kamsdonga@gmail.com>
2024-10-28 17:16:05 +0000

    au

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,3 +32,17 @@ class Server:
         data = self.dataset()
 
         return data[start_index:end_index] if start_index < len(data) else []
+
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+       """ Calculate the start and end index for a given page and page size.
+       Args:
+           page (int): The current page number (1-indexed).
+           page_size (int): The number of items per page.
+    
+       Returns:
+           Tuple[int, int]: A tuple containing the start index and end index.
+       """
+       start_index = (page - 1) * page_size
+       end_index = page * page_size
+       return (start_index, end_index)
