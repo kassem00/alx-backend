c2196e8d7f842123947a7b13b5fc9741b41e40fe
kassem00 <kamsdonga@gmail.com>
2024-10-29 13:02:47 +0000

    au

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+""" Module that defines the index_range function. """
+
 import csv
 from typing import List, Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Calculate the start and end index for a given page and page size."""
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names."""
